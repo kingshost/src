8e297f071252943c7a9b0e0c0727eedd2f856cb8
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2018-05-13 16:24:41 +0300

    Automatic predeployment commit

--- pages/book-one.js
+++ pages/book-one.js
@@ -67,7 +67,7 @@ const BookOnePage = () => (
             <MarketLink
               title={`${MARKET_LINK_TITLE_PREFIX}Apple iBooks`}
               srcPrefix="ibooks"
-              href="https://itunes.apple.com/us/book/kings-host-book-one/id1383931942"
+              href="https://itunes.apple.com/ro/book/kings-host-book-one/id1383931942?mt=11"
             />
           </MarketLinks>
           <MarketLinks title="Read it online on">
